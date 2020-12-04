7829e9ba43419e8ab7acbad6ccc486fe5d3d2bc3
Ratul Roy <ratulroy20@gmail.com>
2020-12-04 18:45:51 +0600

    Update index.php
    trying to make it mobile responsive

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ if(!isset($_COOKIE[$cookie_name])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Daily Journal</title>
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.css">
     <style>
